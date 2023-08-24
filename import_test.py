6629572b1449e1a68644ffa623c60533fe5c4c30
Felix-Okeyo <felixokeyo20@gmail.com>
2023-08-24 10:17:25 +0300

    pending

---
+++ import_test.py
@@ -0,0 +1,14 @@
+from package1.module1 import function1
+function1()
+
+from package2.subpackage1.module6 import function1
+function1()
+
+from relative_package.subpackage1.subpackage2.module1 import function1
+function1()
+
+from relative_package.subpackage1.module3 import function1
+function1
+
+from relative_package.subpackage1.subpackage2.module2 import function1
+function1()
